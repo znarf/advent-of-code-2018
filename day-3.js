f7d06f8a82a4c8028083d5d8d0d0901cc26fda11
François Hodierne <francois@hodierne.net>
2018-12-03 17:52:56 +0100

    day 3 part one - faster version

--- day-3.js
+++ day-3.js
@@ -15,51 +15,41 @@ const testEntries = ['#1 @ 1,3: 4x4', '#2 @ 3,1: 4x4', '#3 @ 5,5: 2x2'];
 const parseEntry = entry => {
   const match = inputFormat.exec(entry);
   if (match) {
-    const id = parseInt(match[1], 10);
-    const position = match[2].split(',').map(s => parseInt(s, 10));
-    const size = match[3].split('x').map(s => parseInt(s, 10));
-    return { id, position, size };
+    const id = Number(match[1]);
+    const [left, top] = match[2].split(',').map(Number);
+    const [width, height] = match[3].split('x').map(Number);
+    return { id, left, top, width, height };
   }
 };
 
-const findMatchingEntries = entries => {
-  const duplicates = {};
-  const length = entries.length;
-  for (let i = 0; i < length; i++) {
-    const firstEntry = entries[i];
-    console.log(`Processing #${firstEntry.id}`);
-    const [firstEntryStartX, firstEntryStartY] = firstEntry.position;
-    const [firstEntryWidth, firstEntryHeight] = firstEntry.size;
-    for (let j = i + 1; j < length; j++) {
-      const secondEntry = entries[j];
-      const [secondEntryStartX, secondEntryStartY] = secondEntry.position;
-      const [secondEntryWidth, secondEntryHeight] = secondEntry.size;
-      for (let ix = firstEntryStartX; ix < firstEntryStartX + firstEntryWidth; ix++) {
-        for (let iy = firstEntryStartY; iy < firstEntryStartY + firstEntryHeight; iy++) {
-          for (let jx = secondEntryStartX; jx < secondEntryStartX + secondEntryWidth; jx++) {
-            for (let jy = secondEntryStartY; jy < secondEntryStartY + secondEntryHeight; jy++) {
-              if (ix === jx && iy === jy) {
-                duplicates[`${ix},${iy}`] = true;
-              }
-            }
-          }
-        }
+const getSquares = entries => {
+  const squares = {};
+  for (const entry of entries) {
+    for (let x = entry.left; x < entry.left + entry.width; x++) {
+      for (let y = entry.top; y < entry.top + entry.height; y++) {
+        const squareKey = `${x},${y}`;
+        squares[squareKey] = (squares[squareKey] || 0) + 1;
       }
     }
   }
-  return Object.keys(duplicates);
+  return squares;
 };
+
+const countOverlappingSquares = squares => Object.values(squares).filter(count => count > 1).length;
 
 const test = () => {
   const parsedTestEntries = testEntries.map(parseEntry);
-  const testDuplicates = findMatchingEntries(parsedTestEntries);
-  assert.equal(testDuplicates.length, 4);
+  const squares = getSquares(parsedTestEntries);
+  assert.equal(countOverlappingSquares(squares), 4);
 };
 
 const run = () => {
   const parsedEntries = entries.map(parseEntry);
-  const duplicates = findMatchingEntries(parsedEntries);
-  console.log('How many square inches of fabric are within two or more claims?', duplicates.length);
+  const squares = getSquares(parsedEntries);
+  console.log(
+    'How many square inches of fabric are within two or more claims?',
+    countOverlappingSquares(squares),
+  );
 };
 
 test();
