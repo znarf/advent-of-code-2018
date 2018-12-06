4f605429ccffa6162a03170acf89e665c2a882b5
François Hodierne <francois@hodierne.net>
2018-12-06 10:47:38 +0100

    day 6 part two

--- day-6.js
+++ day-6.js
@@ -20,7 +20,7 @@ const arrayUnique = array => [...new Set(array)];
 
 const parseCoordinates = input => input.map(([x, y], index) => ({ x, y, index }));
 
-const getLargestNonInfiniteArea = input => {
+const getCoordinateWithLargestNonInfiniteArea = input => {
   const coordinates = parseCoordinates(input);
 
   const xMax = Math.max(...coordinates.map(c => c.x));
@@ -39,32 +39,58 @@ const getLargestNonInfiniteArea = input => {
     }
   }
 
-  const infiniteCoordinatesIndexes = arrayUnique(
+  const coordinateWithInfiniteAreaIndexes = arrayUnique(
     points
       .filter(p => p.x === 0 || p.x === xMax || p.y === 0 || p.y === yMax)
       .map(p => p.closestCoordinateIndex),
   );
 
-  const largestNonInfiniteCoordinates = coordinates
-    .filter(c => !infiniteCoordinatesIndexes.includes(c.index))
+  const coordinateWithLargestNonInfiniteArea = coordinates
+    .filter(c => !coordinateWithInfiniteAreaIndexes.includes(c.index))
     .map(c => {
       const count = points.filter(p => p.closestCoordinateIndex === c.index).length;
       return { ...c, count };
     })
-    .sort((a, b) => b.count - a.count)
-    .shift();
+    .sort((a, b) => b.count - a.count);
 
-  return largestNonInfiniteCoordinates;
+  return coordinateWithLargestNonInfiniteArea.shift();
+};
+
+const getSafePoints = (input, thresold) => {
+  const coordinates = parseCoordinates(input);
+
+  const xMax = Math.max(...coordinates.map(c => c.x));
+  const yMax = Math.max(...coordinates.map(c => c.y));
+
+  const points = [];
+
+  for (let x = 0; x <= xMax; x++) {
+    for (let y = 0; y <= yMax; y++) {
+      const totalDistance = coordinates.reduce(
+        (total, c) => total + Math.abs(x - c.x) + Math.abs(y - c.y),
+        0,
+      );
+      if (totalDistance < thresold) {
+        points.push({ x, y, totalDistance });
+      }
+    }
+  }
+
+  return points;
 };
 
 const test = () => {
-  const largestNonInfiniteArea = getLargestNonInfiniteArea(testInput);
+  const largestNonInfiniteArea = getCoordinateWithLargestNonInfiniteArea(testInput);
   assert.equal(largestNonInfiniteArea.count, 17);
+  const safePoints = getSafePoints(testInput, 32);
+  assert.equal(safePoints.length, 16);
 };
 
 const run = () => {
-  const largestNonInfiniteArea = getLargestNonInfiniteArea(input);
+  const largestNonInfiniteArea = getCoordinateWithLargestNonInfiniteArea(input);
   console.log(largestNonInfiniteArea.count);
+  const safePoints = getSafePoints(input, 10000);
+  console.log(safePoints.length);
 };
 
 test();
