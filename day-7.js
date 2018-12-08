6022aead3e9a06ec872505120ed4be5e3047745e
François Hodierne <francois@hodierne.net>
2018-12-08 10:19:38 +0100

    day 7 part two

--- day-7.js
+++ day-7.js
@@ -20,6 +20,8 @@ const testInput = [
   'Step F must be finished before step E can begin.',
 ];
 
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
 const arrayFlatten = array => [].concat(...array);
 
 const arrayUnique = array => [...new Set(array)];
@@ -39,40 +41,97 @@ const getParsedSteps = input => {
   return steps;
 };
 
-const completeSteps = steps => {
-  const stepIds = arrayUnique([
+const getStepIds = steps => {
+  return (stepIds = arrayUnique([
     ...Object.keys(steps),
     ...arrayFlatten(Object.values(steps)),
-  ]).sort();
+  ]).sort());
+};
 
-  const completedSteps = [];
+const getStepsPlan = steps => {
+  const stepIds = getStepIds(steps);
+
+  const stepsPlan = [];
 
   do {
     for (const char of stepIds) {
-      if (completedSteps.includes(char)) {
+      if (stepsPlan.includes(char)) {
         continue;
       }
-      if (!steps[char] || difference(steps[char], completedSteps).length === 0) {
-        completedSteps.push(char);
+      if (!steps[char] || difference(steps[char], stepsPlan).length === 0) {
+        stepsPlan.push(char);
         break;
       }
     }
-  } while (completedSteps.length < stepIds.length);
+  } while (stepsPlan.length < stepIds.length);
 
-  return completedSteps;
+  return stepsPlan;
+};
+
+const executeSteps = (steps, baseStepDuration = 0, nWorkers = 2) => {
+  const stepIds = getStepIds(steps);
+
+  let duration = 0;
+
+  const workers = [];
+  const executedSteps = [];
+
+  do {
+    stepIds: for (const stepId of stepIds) {
+      // Item already processed
+      if (executedSteps.includes(stepId)) {
+        continue stepIds;
+      }
+      // Item not processable
+      if (steps[stepId] && difference(steps[stepId], executedSteps).length > 0) {
+        continue stepIds;
+      }
+      // Item already processing
+      for (let i = 0; i < nWorkers; i++) {
+        if (workers[i] && workers[i].stepId === stepId) {
+          continue stepIds;
+        }
+      }
+      // Try to find a worker for item
+      for (let i = 0; i < nWorkers; i++) {
+        if (!workers[i]) {
+          workers[i] = { stepId, remaining: alphabet.indexOf(stepId) + 1 + baseStepDuration };
+          continue stepIds;
+        }
+      }
+    }
+
+    // Update workers status
+    for (let i = 0; i < nWorkers; i++) {
+      if (workers[i]) {
+        workers[i].remaining--;
+        if (workers[i].remaining === 0) {
+          executedSteps.push(workers[i].stepId);
+          workers[i] = null;
+        }
+      }
+    }
+
+    duration++;
+  } while (executedSteps.length < stepIds.length);
+
+  return { totalDuration: duration };
 };
 
 const test = () => {
   const parsedSteps = getParsedSteps(testInput);
-  const completedSteps = completeSteps(parsedSteps);
-  assert.equal(completedSteps.join(''), 'CABDFE');
+  const stepsPlan = getStepsPlan(parsedSteps);
+  assert.equal(stepsPlan.join(''), 'CABDFE');
+  const result = executeSteps(parsedSteps, 0, 2);
+  assert.equal(result.totalDuration, 15);
 };
 
 const run = () => {
   const parsedSteps = getParsedSteps(input);
-  console.log(parsedSteps);
-  const completedSteps = completeSteps(parsedSteps);
-  console.log(completedSteps.join(''));
+  const stepsPlan = getStepsPlan(parsedSteps);
+  console.log(stepsPlan.join(''));
+  const result = executeSteps(parsedSteps, 60, 5);
+  console.log(result.totalDuration);
 };
 
 test();
